db1f31be6ce9e3595e11c475ec4ec149c98a7dc1
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2026-09-05 20:01:23 +0330

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -23,7 +23,7 @@ st.markdown(
     """
     <div style="margin-top: 20px; margin-bottom: 20px; font-size: 16px;">
         <div style="display: flex; align-items: center; gap: 10px;">
-            <img src="https://pbs.twimg.com/profile_images/1841479747332608000/bindDGZQ_400x400.jpg" alt="Eman Raz" style="width:25px; height:25px; border-radius: 50%;">
+            <img src="https://pbs.twimg.com/profile_images/2060406047391559681/sA9zPNKM_400x400.jpg" alt="Eman Raz" style="width:25px; height:25px; border-radius: 50%;">
             <span>Built by: <a href="https://x.com/0xeman_raz" target="_blank">Eman Raz</a></span>
         </div>
         
@@ -32,39 +32,14 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# --- Info Box ---
-st.markdown(
-    """
-    <div style="background-color: #e6fa36; padding: 15px; border-radius: 10px; border: 1px solid #ffd700;">
-        Squid Router is a cross-chain liquidity and messaging protocol built on the Axelar Network, designed to facilitate seamless token swaps, 
-        transfers, and smart contract interactions across multiple blockchains. 
-        Squid enables users to swap any token across over 90 blockchains (e.g., Ethereum, Polygon, Arbitrum, Solana, Bitcoin) in a single click.
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
-# --- Reference and Rebuild Info ---
-st.markdown(
-    """
-    <div style="margin-top: 20px; margin-bottom: 20px; font-size: 16px;">
-        <div style="display: flex; align-items: center; gap: 10px;">
-            <img src="https://pbs.twimg.com/profile_images/1856738793325268992/OouKI10c_400x400.jpg" alt="Flipside" style="width:25px; height:25px; border-radius: 50%;">
-            <span>Data Powered by: <a href="https://flipsidecrypto.xyz/home/" target="_blank">Flipside</a></span>
-        </div>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+
+
 
 # --- Links with Logos ---
 st.markdown(
     """
     <div style="font-size: 16px;">
-        <div style="display: flex; align-items: center; gap: 10px;">
-            <img src="https://axelarscan.io/logos/logo.png" alt="Axelar" style="width:20px; height:20px;">
-            <a href="https://www.axelar.network/" target="_blank">Axelar Website</a>
-        </div>
         <div style="display: flex; align-items: center; gap: 10px;">
             <img src="https://axelarscan.io/logos/accounts/squid.svg" alt="Squid" style="width:20px; height:20px;">
             <a href="https://www.squidrouter.com/" target="_blank">Squid Website</a>
