182f1f9b703b17c9ca507f4bbcf6dddec13fd24b
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2026-09-08 18:09:50 +0330

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -755,8 +755,10 @@ with time_range_col:
         "Select time range:",
         options=[
             "7d",
+            "mtd",
             "30d",
             "90d",
+            "qtd",
             "ytd",
             "1y",
             "all"
